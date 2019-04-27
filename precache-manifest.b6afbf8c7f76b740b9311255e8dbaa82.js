62cb8bc6248d7c6b30497b1da847ca34b152b1b0
Shirley Wong <shirleywsywong@gmail.com>
2019-04-27 14:39:46 -0400

    Updates

---
+++ precache-manifest.b6afbf8c7f76b740b9311255e8dbaa82.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4afb2bd7bd54f065ca406ec4acbf0162",
+    "url": "/javascript-project-2/static/media/loading.4afb2bd7.gif"
+  },
+  {
+    "revision": "fddd1c68b8b1a7d0ed4a69e817db286e",
+    "url": "/javascript-project-2/static/media/hero-nose.fddd1c68.jpg"
+  },
+  {
+    "revision": "e5637b4c64a7e9b9ecb64814336aa66a",
+    "url": "/javascript-project-2/static/media/hero-hp2.e5637b4c.jpg"
+  },
+  {
+    "revision": "6d5a358c6fc022685b30",
+    "url": "/javascript-project-2/static/js/runtime~main.9a7ce735.js"
+  },
+  {
+    "revision": "19b5c7ab1fef4e9ca24f",
+    "url": "/javascript-project-2/static/js/main.83d10f32.chunk.js"
+  },
+  {
+    "revision": "fd326e0af51560db1601",
+    "url": "/javascript-project-2/static/js/2.bf284bdb.chunk.js"
+  },
+  {
+    "revision": "19b5c7ab1fef4e9ca24f",
+    "url": "/javascript-project-2/static/css/main.f1d3a4ce.chunk.css"
+  },
+  {
+    "revision": "a82cd589c3f029de5a40ef68fda1ebfe",
+    "url": "/javascript-project-2/index.html"
+  }
+];
